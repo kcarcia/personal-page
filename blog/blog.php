eba0df8869349e46c1273a632c43842c30631c8f
Kaitlyn Carcia <kaitlyn_carcia@student.uml.edu>
2015-01-09 22:35:08 -0500

    Added blog.

---
+++ blog/blog.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<link href="./css/arrange.css" rel="stylesheet">		
+		<link href="./css/main.css" rel="stylesheet">
+		
+		<title>Kaitlyn Carcia</title>
+	</head>
+
+	<body>
+		<header>
+			<!-- <nav>
+				<div class="page">
+					<div class="nav-button"><a href="#">About</a></div>
+					<div class="nav-button"><a href="#">Blog</a></div>
+					<div class="nav-button"><a href="#">Contact</a></div>
+				</div>
+			</nav> -->
+			
+ 			<!-- Will be nav -->
+			<div style="background: #628A9C; padding: 25px 0 25px 0;"></div>
+ 			
+ 
+ 			<!-- Will be "logo" -->
+			<div style="background: #3C6C81; padding: 100px 0 100px 0;"></div>
+		</header>
+		
+		<div class="main">
+			<div class="page">
+				<h1>Blog</h1>
+
+			</div>
+		</div>
+	</body>
+<html>
+
+<!-- <p>&copy; 2014 Kaitlyn Carcia</p> -->
---
+++ blog/source/_posts/testingwee-2.md
@@ -0,0 +1,6 @@
+layout:
+  - layout
+title: testingwee
+date: 2015-01-07 23:17:14
+tags:
+---
